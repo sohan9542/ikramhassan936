4c8ed63b8a6523ee2f9a8067320ad6caa2f40515
sohan9542 <marufislam9542@gmail.com>
2022-02-26 18:08:40 +0600

    latest commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from "react";
-import Timeline from "./component/Timeline";
-import VedioList from "./component/VedioList";
-
 const App = () => {
   return (
     <div className=" min-h-screen">
-        {/* <h1 className="text-center w-full text-3xl  text-gray-900 font-bold py-4">Timeline</h1>
-      <Timeline /> */}
-      <VedioList />
+        Hello world
     </div>
   );
 };
